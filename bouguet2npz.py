124929bc61a7a9fa5f34913a6787cf370c0eb5ca
Scott Livingston <slivingston@caltech.edu>
2012-12-06 13:31:55 -0800

    Small script for moving camera parameters from MATLAB file to NumPy NPZ

---
+++ bouguet2npz.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+Extract several calibration parameters obtained using J.-Y. Bouguet's
+Camera Calibration Toolbox for Matlab and save them into a NumPy NPZ file.
+
+SCL; 13 Nov 2012.
+"""
+
+import sys
+import numpy as np
+from scipy.io import loadmat
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2 and len(sys.argv) != 3:
+        print "Usage: bouguet2npz.py INPUT.mat [OUTPUT.npz]"
+        exit(1)
+
+    matdata = loadmat(sys.argv[1])
+    fc = matdata["fc"]  # focal length
+    cc = matdata["cc"]  # principal point
+    kc = matdata["kc"]  # radial and tangential distortion
+
+    if len(sys.argv) == 2:
+        outname = sys.argv[1]+".npz"
+    else:
+        outname = sys.argv[2]
+    np.savez(outname, fc=fc, cc=cc, kc=kc)
